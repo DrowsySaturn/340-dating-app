932b15c95a3101e53750da1b3f8882135677ed24
Jonathan Cooper <drowsysaturn@gmail.com>
2018-11-20 14:06:27 -0500

    Add more requests to client and server

--- android-studio-project/app/src/main/java/com/datingapp/client/net/GenericServerConnector.java
+++ android-studio-project/app/src/main/java/com/datingapp/client/net/GenericServerConnector.java
@@ -1,8 +1,4 @@
 package com.datingapp.client.net;
-
-import com.datingapp.shared.dataobjects.Profile;
-import com.datingapp.shared.datapersistence.LoginConfirmation;
-
 /**
  * The purpose of the ServerConnector is to download or upload JSON with the intent of communicating
  * with the database or the server. All communication with the server goes through this object.
@@ -11,10 +7,18 @@ import com.datingapp.shared.datapersistence.LoginConfirmation;
  * @version oct-18-2018
  */
 
+import com.datingapp.shared.dataobjects.LoginInformation;
+import com.datingapp.shared.dataobjects.Profile;
+import com.datingapp.shared.datapersistence.LoginConfirmation;
+
 public abstract class GenericServerConnector {
     public abstract Profile loadProfileById(long _profileId) throws DatingNetworkException;
 
-    public abstract void likeProfile(long _profileId) throws DatingNetworkException;
+    public abstract void likeProfile(long _likerId, long _likedId, String _username, String _sessionKey) throws DatingNetworkException;
 
     public abstract LoginConfirmation validateLogin(String _email, String _password) throws DatingNetworkException;
+
+    public abstract Profile[] getMatches(String _username, String _sessionKey) throws DatingNetworkException;
+
+    public abstract void registerProfile(LoginInformation _loginInformation) throws DatingNetworkException;
 }
--- android-studio-project/app/src/main/java/com/datingapp/client/net/ServerCommunicator.java
+++ android-studio-project/app/src/main/java/com/datingapp/client/net/ServerCommunicator.java
@@ -1,16 +1,35 @@
 package com.datingapp.client.net;
+/**
+ * This class facilitates communication with the server for the client.
+ *
+ * @author Jonathan Cooper
+ * @version nov-20-2018
+ */
 
+import com.datingapp.shared.dataobjects.LoginInformation;
+import com.datingapp.shared.dataobjects.Profile;
 import com.datingapp.shared.datapersistence.LoginConfirmation;
-import com.datingapp.shared.datapersistence.Profile;
 
 public class ServerCommunicator {
     private GenericServerConnector serverConnector = new HttpServerConnector();
 
-    public Profile loadProfileById(int _id) throws DatingNetworkException {
-        return serverConnector.loadProfileById(_id);
+    public void registerProfile(LoginInformation _registrationData) throws DatingNetworkException {
+        this.serverConnector.registerProfile(_registrationData);
+    }
+
+    public Profile loadProfileById(long _id) throws DatingNetworkException {
+        return this.serverConnector.loadProfileById(_id);
+    }
+
+    public void likeProfile(long _likerId, long _likedId, String _username, String _sessionKey) throws DatingNetworkException {
+        this.serverConnector.likeProfile(_likerId, _likedId, _username, _sessionKey);
     }
 
     public LoginConfirmation validateLogin(String _email, String _password) throws DatingNetworkException {
-        return serverConnector.validateLogin(_email, _password);
+        return this.serverConnector.validateLogin(_email, _password);
+    }
+
+    public Profile[] getMatches(String _username, String _sessionKey) throws DatingNetworkException {
+        return this.serverConnector.getMatches(_username, _sessionKey);
     }
 }
